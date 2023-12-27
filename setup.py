1092836bc3d5e73c04b9f50c7b324882b13d01bc
Ashwin Pillay <ashwin240298@gmail.com>
2023-12-27 03:00:52 -0500

    - added setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+# Derived from: https://github.com/iver56/audiomentations/blob/main/setup.py
+
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='midiogre',
+    packages=find_packages(exclude=["demo", "tests"]),
+    version='0.1.0',
+    license='MIT',
+    description='The On-the-fly MIDI Data Augmentation Library!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ashwin Pillay',
+    url='https://github.com/a-pillay/MIDIOgre',
+    keywords=['MIDI', 'Audio', 'Machine Learning', 'Data Augmentation', 'Deep Learning'],
+    install_requires=[
+        "numpy>=1.21.6",
+        "pretty-midi>=0.2.10",
+        "torch>=1.13.1",
+    ],
+    extras_require={
+        "extras": [
+            "matplotlib>=3.5.3",
+        ]
+    },
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Multimedia",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    project_urls={
+        # "Documentation": "TBA",  # TODO
+        # "Changelog": "TBA",  # TODO
+        "Issue Tracker": "https://github.com/a-pillay/MIDIOgre/issues",
+    },
+)
