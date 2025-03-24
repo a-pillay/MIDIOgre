80cf8318316c18ab545be868d698cf2f900f6415
Ashwin Pillay <ashwin240298@gmail.com>
2025-03-23 20:54:05 -0700

    - Changed version scheme to release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(exclude=["demo", "tests"]),
     use_scm_version={
         "write_to": "midiogre/_version.py",
-        "version_scheme": "post-release",
+        "version_scheme": "release",
     },
     setup_requires=['setuptools_scm'],
     license='MIT',
